41ab129f7302fe40747ca699b2a19b92f68c5a99
brainDamage153 <pablo.vc153@gmail.com>
2017-07-12 01:57:31 -0400

    12072017 - InicioProyecto
    Kickoff

---
+++ CohabManager/IngresoLuchadores.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CohabManager
+{
+    public partial class IngresoLuchadores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtRut.Attributes.Add("placeHolder", "Rut");
+            txtNombre.Attributes.Add("placeHolder", "Nombre");
+            txtApellido.Attributes.Add("placeholder","Apellido");
+            txtFechaNac.Attributes.Add("placeholder", "Fecha de Nacimiento");
+            txtCinturon.Attributes.Add("placeholder", "Cinturon");//cambiar por combobox
+
+        }
+    }
+}
